8383ebed302c771b804cad479441e80a5232b39a
Carlos Tuñón <camotubi@gmail.com>
2017-10-21 18:06:48 -0500

    Add Person class definition

---
+++ person.php
@@ -0,0 +1,50 @@
+<?php
+class Person {
+  private $name;
+          $lastName;
+          $birthdate;
+          $email;
+          $interests[];
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function setName($name){
+    $this->name = $name;
+  }
+
+  public function getLastName(){
+    return $this->lastName;
+  }
+
+  public function setLastName($lastName){
+    $this->lastName = $lastName;
+  }
+
+  public function getBirthdate(){
+    return $this->birthdate;
+  }
+
+  public function setBirthdate($birthdate){
+    $this->birthdate = $birthdate;
+  }
+
+  public function getEmail(){
+    return $this->email;
+  }
+
+  public function setEmail($email){
+    $this->email = $email;
+  }
+
+  public function getInterests(){
+    return $this->intersts;
+  }
+
+  public function setInterests($interests[]){
+    $this->interests = $interests;
+  }
+}
+
+ ?>
