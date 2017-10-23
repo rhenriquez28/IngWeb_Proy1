45c7944d94f69ed105024edf2082f9b6d95cfbc6
Camotubi <camotubi@gmail.com>
2017-10-23 00:11:57 -0500

    fix require issues and change json access from object to an array

--- productsController.php
+++ productsController.php
@@ -1,16 +1,19 @@
 <?php
-require('product.php');
-define("PRODUCTS_DEF_FILE","");
-
+require_once('product.php');
+require_once('productView.php');
+require_once('productsGalleryView.php');
+define("PRODUCTS_DEF_FILE","products.json");
 $tmpJsonObjs = json_decode(file_get_contents(PRODUCTS_DEF_FILE),true);
 $products = array();
-foreach($tmpJsonObjs->products as $tmpJsonProduct)
+foreach($tmpJsonObjs["products"] as $tmpJsonProduct)
 {
   $tmpProduct = new Product();
   $tmpProduct->withJSON($tmpJsonProduct);
   array_push($products,$tmpProduct);
 }
-
+//$pView = new ProductView($products[0]);
+$pView = new ProductsGalleryView($products);
+$pView->render();
 
 
 ?>
