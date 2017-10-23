9280c22b6096a02dd7749ebf2f104ca30e404cf4
Camotubi <camotubi@gmail.com>
2017-10-23 02:03:35 -0500

    add a flow of actions with action variable

--- productsController.php
+++ productsController.php
@@ -11,9 +11,56 @@ foreach($tmpJsonObjs["products"] as $tmpJsonProduct)
   $tmpProduct->withJSON($tmpJsonProduct);
   array_push($products,$tmpProduct);
 }
-//$pView = new ProductView($products[0]);
-$pView = new ProductsGalleryView($products);
-$pView->render();
+if(isset($_GET["action"]))
+{
+	$action = $_GET["action"];
+}
+else
+{
+	$action = "gallery";
+}
+switch($action)
+{
+	case "gallery":
+	$pView = new ProductsGalleryView($products);
+	$pView->render();
+	break;
+	case "showProduct":
+		$productID = 0;
+		if(isset($_GET["productID"]))
+		{
+			$productID = intval($_GET["productID"]);
+		}
+	$pView = new ProductView($products[$productID]);
+	$pView->render();
+	break;
+	case "search":
+		$q="";
+		if(isset($_GET["q"]))
+		{
+		 $q = $_GET["q"];
+		}
+		$q = strtolower($q);
+		$len=strlen($q);
+		$hint = "";
+		$i=0;
+		$productID=0;
+    		foreach($products as $product) {
+			if (stristr($product->getName(), $q)) {
+				$productID=$i;
+				$hint = $product->getName();
+				}
+			$i=$i+1;
+			    }
+		if ($hint=="") {
+  $response="no suggestion";
+} else {
+  $response='<a style="color:grey" href=productsController.php?action=showProduct&productID='.$productID.'>'.$hint.'</a>';
+}
 
+//output the response
+echo '<p style="color:grey">'.$response.'</p>';
+		break;
+}
 
 ?>
