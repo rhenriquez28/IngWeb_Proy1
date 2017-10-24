c1985def69bd670be838bf30dced5daa96f6894b
Camotubi <camotubi@gmail.com>
2017-10-23 19:19:55 -0500

    add name of company

--- View.php
+++ View.php
@@ -98,7 +98,7 @@ public function __construct()
     {
 	    $navbar ='
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Hz.Tek</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
